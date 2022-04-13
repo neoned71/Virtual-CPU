2b53b57ac2b89d240587d70941956cca452d28ae
Piyush Shukla <neoned71@gmail.com>
2022-04-14 03:13:27 +0530

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -1,5 +1,5 @@
-#include "instructions.hpp"
 #include<iostream>
+#include "instructions.hpp"
 #include<vector>
 
 class CPU{
@@ -8,6 +8,7 @@ class CPU{
     int ip,sp,bp;
 
     int flag=0;
+    bool debugging=false;
 
     //memory regions
     std::vector<int> stack;
@@ -15,47 +16,76 @@ class CPU{
     std::vector<int> data;
 
     public:
-        CPU(std::vector<int>&& instructions,int main_address,int datasize=100,int stacksize=200):sp(-1),bp(0),ip(main_address),stack(stacksize),data(datasize),code(instructions) {
-            // std::cout<< data.size() <<std::endl;
-            // std::cout<< code.size() <<std::endl;
+        CPU(std::vector<int>&& instructions,int main_address,int datasize=100,int stacksize=20):sp(-1),bp(0),ip(main_address),stack(stacksize),data(datasize),code(instructions) {
         }
 
         void run()
         {
             std::cout<<"Cpu Started" << std::endl;
-            ip=0;
+            // ip=0;
             int value;
+            int i;
             while(ip<code.size())
             {
                 int instruction = fetch();//fetch
+                //std::cout<<instruction<<" ";
                 switch(instruction)//decode
                 {
                     case IPUSH:
                         value=code[ip++];
                         stack[++sp] = value;
-                         std::cout<<"Pushing now: "<< value <<std::endl;
-                        // execute
+                        //std::cout<<"Pushing now: "<< value <<ip<<std::endl;
+                        printStack();
                         break;
 
                     case IADD:
                         stack[++sp]=code[ip++]+code[ip++];
-                        //  = value;
-                         std::cout<<"Adding Ans: "<< stack[sp] <<std::endl;
-                        // execute
+                        printStack();
                         break;
 
                     case ISUB:
                         stack[++sp]=code[ip++]-code[ip++];
-                        //  = value;
-                         std::cout<<"Subtracting Ans: "<< stack[sp] <<std::endl;
-                        // execute
+                        printStack();
                         break;
 
                     case IMUL:
                         stack[++sp]=code[ip++]*code[ip++];
-                        //  = value;
-                         std::cout<<"Multiplying Ans: "<< stack[sp] <<std::endl;
-                        // execute
+                        printStack();
+                        break;
+
+                    case IADDS:// instruction for when operands are on the stack! IADDS 0 1
+                        value=stack[bp-code[ip++]]+stack[bp-code[ip++]];
+                        std::cout<<value;
+                        stack[++sp]=value;
+                        printStack();
+                        break;
+
+                    case ISUBS:// instruction for when operands are on the stack! ISUBS 0 1
+                        stack[++sp]=stack[bp-code[ip++]]-stack[bp-code[ip++]];
+                        printStack();
+                        break;
+
+                    case IMULS:// instruction for when operands are on the stack! IMULS 0 1
+                        stack[sp]=stack[bp-code[ip++]]*stack[bp-code[ip++]];
+                        printStack();
+                        break;
+
+                    case ICMPS: // ICMP -1 3. relative to the base pointer
+                        value=stack[bp-code[ip++]];
+                        i=stack[bp-code[ip]];
+                        if(value == i)
+                        {
+                            flag = 0;   
+                        }
+                        else if(value > i)
+                        {
+                            flag =1;
+                        }
+                        else{
+                            flag=-1;
+                        }
+                        ip++;
+                        printStack();
                         break;
 
                     case ICMP:
@@ -72,94 +102,86 @@ class CPU{
                             flag=-1;
                         }
                         ip++;
-                        //  = value;
-                         std::cout<<"Multiplying Ans: "<< stack[sp] <<std::endl;
-                        // execute
+                        printStack();
                         break;
 
                     case JUMP:
                         ip=code[ip];
-                        //  = value;
-                        std::cout<<"Jumping to: "<< ip <<std::endl;
-                        // execute
+                        printStack();
                         break;
 
                     case JEQ:
-                        if(flag=0)
+                        if(flag==0)
                         {
                             ip=code[ip];
-                        //  = value;
-                            std::cout<<"Jumping with flag 0 to: "<< ip <<std::endl;
                         }
                         else{
                             ip++;
                         }
                         // execute
+                        printStack();
                         break;
                     
                     case JLT:
                         if(flag<0)
                         {
                             ip=code[ip];
-                        //  = value;
-                            std::cout<<"Jumping with flag < 0 to: "<< ip <<std::endl;
                         }
                         else{
                             ip++;
                         }
-                        // execute
+                        printStack();
                         break;
                     
                     case JGT:
-                        if(flag=0)
+                        if(flag>0)
                         {
                             ip=code[ip];
-                        //  = value;
-                            std::cout<<"Jumping with flag > 0 to: "<< ip <<std::endl;
                         }
                         else{
                             ip++;
                         }
-                        // execute
+                        printStack();
                         break;
 
                     case PRINT:
                         if(sp>-1)
                         {
-                            std::cout << "Stack top: "<<stack[sp] <<std::endl;
+                            std::cout << "Stack top: "<<stack[sp] <<" and flag: "<<flag<<std::endl;
                         } 
                         else{
-                            std::cout << "Stack Empty!"<<std::endl;
+                            std::cout << "Stack Empty! and flag: "<<flag<<std::endl;
                         }
+                        printStack();
                         break;
+
 
                     case HALT:
                         std::cout<<"Halting now"<<std::endl;
+                        printStack();
                         break;
                     
                     case POP:
                         std::cout<<"Popping now: "<<stack[sp]<<std::endl;
                         sp--;
+                        printStack();
                         break;    
 
                     case STORE:
-                        std::cout<<"Storing now to data address: "<<code[ip]<<std::endl;
                         data[code[ip++]]=stack[sp--];
-                        std::cout<<"Data:" <<data[code[ip-1]] << std::endl;
+                        printStack();
                         break;
 
                     case LOAD:
-                        std::cout<<"Loading now from data address: "<<code[ip]<<std::endl;
                         stack[++sp]=data[code[ip++]];
-                        std::cout<<"stack:" <<stack[sp] << std::endl;
-                        // sp--;
+                        printStack();
                         break; 
 
                     case LOCAL_LOAD:
                         std::cout<<"Local Loading from offset: "<<code[ip]<<std::endl;
                         stack[++sp]=data[bp+code[ip++]];
                         std::cout<<"stack:" <<stack[sp] << std::endl;
-                        // sp--;
+                        printStack();
                         break; 
 
                     //functions=> syntax-> `CALL address #arguments`
@@ -167,9 +189,10 @@ class CPU{
                         value = code[ip++];
                         stack[++sp] = code[ip++];;//number of arguments
                         stack[++sp] = bp;//base pointer
-                        stack[++sp] = ip;//return address
+                        stack[++sp] = ip;//return address: address of the next instruction after call
                         bp=sp;
                         ip = value;
+                        printStack();
                         break;
 
                     //clear everything upto the last argument and put returned value on the top of the stack!
@@ -180,6 +203,7 @@ class CPU{
                         bp=stack[sp--];
                         sp -= stack[sp--];
                         stack[++sp] = return_value;
+                        printStack();
                         break;
                 }
             }
@@ -190,5 +214,19 @@ class CPU{
         {
             return code[ip++];
         }
+
+        void printStack()
+        {
+            if(debugging)
+            {
+                for(auto i : stack){
+                std::cout<<i<<',';
+            }
+            std::cout<<"Ip:"<<ip<<" bp:"<<bp<<" flag:"<<flag;
+            std::cout<<std::endl;
+            }
+        }
+
+
 
 };
